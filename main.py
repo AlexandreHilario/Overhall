9886974fc3f838a59c2d983308806a0f1ff25f79
Alexandre Hilario <alexandreds969@gmail.com>
2024-11-16 11:50:55 -0300

    resumo

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pytubefix
 from pytubefix.cli import on_progress
 import os
 import whisper
+from transformers import pipeline
+
 
 def download_audio(url, output_folder = "audio_files"):
     if not os.path.exists(output_folder):
@@ -20,9 +22,27 @@ def transcription_audio(audio_path):
     result = model.transcribe(audio_path)
     return result['text']
 
+def summarize_text(text):
+    summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
+    
+    chunk_size = 1024 
+    chunks = [text[i:i+chunk_size] for i in range(0, len(text), chunk_size)]
+    
+    summaries = []
+    for chunk in chunks:
+        summary = summarizer(chunk, max_length=130, min_length=30, do_sample=False)
+        summaries.append(summary[0]['summary_text'])
+    
+    final_summary = ' '.join(summaries)
+    return final_summary
 
 url = input("Coloque a url: ")
 audio_path = download_audio(url)
 
-transcription = transcription_audio(audio_path)
-print(transcription)
+try:
+    transcription = transcription_audio(audio_path)
+    summary = summarize_text(transcription)
+    print(summary)
+finally:
+    if os.path.exists(audio_path):
+        os.remove(audio_path)
